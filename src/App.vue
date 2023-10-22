f2a8fc00bd71a8c4691b6556486b95874a12bd2f
MrMacaroli <145093191+MrMacaroli@users.noreply.github.com>
2023-10-22 00:03:55 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -118,7 +118,7 @@ export default {
         },
         {
           "callsign": "Muninn",
-          "alias": "Pashije Vojteschka",
+          "alias": "NOT FOUND",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///UNS-ARGOT-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Chomolungma",
